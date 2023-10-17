f97e2027b88eb5330519e29051000c64c27e09b3
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-16 19:48:10 -0600

    feat: adding routing to the index path

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './App.css'
 import Home from './Components/Home'
 import Footer from './Components/Layout/Footer'
 import Header from './Components/Layout/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   )
