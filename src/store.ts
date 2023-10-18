a44cf78e1ad0e51f0e8bc80c7fb71fbcefc02d54
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-18 17:31:38 -0600

    feat: now the store has products reducer and 2 new types

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { productReducer } from './slices/productSlice'
 
-export default configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: {
+    products: productReducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
