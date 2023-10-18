8243cf1bd8da8f1e29a7602f97e02dbd97ada438
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-18 17:33:27 -0600

    feat: product slice was created

---
+++ src/slices/productSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { getProducts } from '../actions/productAction'
+
+interface ProductState {
+  products: never[]
+  loading: boolean
+  error: null | string
+}
+
+export const initialState: ProductState = {
+  products: [],
+  loading: false,
+  error: null,
+}
+
+export const productSlice = createSlice({
+  name: 'product',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getProducts.pending, (state) => {
+      state.loading = true
+      state.error = null
+    }),
+      builder.addCase(getProducts.fulfilled, (state, action) => {
+        state.loading = false
+        state.products = action.payload
+        state.error = null
+      }),
+      builder.addCase(getProducts.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.error.message!
+      })
+  },
+})
+
+export const productReducer = productSlice.reducer
