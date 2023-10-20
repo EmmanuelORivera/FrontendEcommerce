f41477db5d0b922fa5875ae99afe0f6d614b3aa5
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-20 16:13:58 -0600

    feat: adding types to the component

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom'
+import { IProduct } from '../../interfaces/IProduct'
 
-const Product = ({ product, col }) => {
+interface ProductProps {
+  product: IProduct
+  col: number
+}
+const Product = ({ product, col }: ProductProps) => {
   const default_image = './images/default_product.png'
   return (
     <div className={`col-sm-12 col-md-6 col-lg-${col} my-3`}>
       <div className="card p-3 rounded">
         <img
           className="card-img-top mx-auto"
-          src={product.images[0] ? product.images[0].url : default_image}
+          src={
+            product.images && product.images.length > 0
+              ? product.images[0].url!
+              : default_image
+          }
         />
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">
