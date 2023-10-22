99eeeb638054f63c81a777bf93547a92f02665ca
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-21 20:33:45 -0600

    refactor: wrapping with provider and using beforeEach global.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,28 @@
-import { expect, describe, test as it } from 'vitest'
+import { expect, describe, test as it, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 describe('App', () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  })
   it('should render the component with the header', () => {
-    render(<App />)
-
     const headerElement = screen.getByTestId('header')
     expect(headerElement).toBeInTheDocument()
   })
 
   it('should render the component with a footer', () => {
-    render(<App />)
-
     const footerElement = screen.getByTestId('footer')
     expect(footerElement).toBeInTheDocument()
   })
 
   it('should render the home component', () => {
-    render(<App />)
-
     const homeComponent = screen.getByTestId('home')
     expect(homeComponent).toBeInTheDocument()
   })
