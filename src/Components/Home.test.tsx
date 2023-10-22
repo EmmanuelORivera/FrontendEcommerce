36c3cf70362210e19477f872e3e16462be38899e
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-21 20:34:19 -0600

    test: creation of tests and refactoring

--- src/Components/Home.test.tsx
+++ src/Components/Home.test.tsx
@@ -1,52 +1,91 @@
+import { rest } from 'msw'
+import server from '../mocks/handlers'
 import { render, screen, waitFor } from '@testing-library/react'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { describe, expect, it, vitest } from 'vitest'
 import { Provider } from 'react-redux'
 import Home from './Home'
 import { store } from '../store'
+import { MemoryRouter } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
+vitest.mock('react-toastify', () => ({
+  toast: {
+    error: vitest.fn(),
+  },
+}))
 describe('Home', () => {
-  beforeEach(() => {
+  it('should render component without a problem', () => {
     render(
       <Provider store={store}>
         <Home />
       </Provider>
     )
   })
-  it('should render component without a problem', () => {})
 
-  it('should render a product image', () => {
-    const imageElement = screen.getByRole('img')
-    expect(imageElement).toBeInTheDocument()
-  })
-
-  it('should render a product title with the correct text', () => {
-    const headingElement = screen.getByRole('heading')
-    expect(headingElement).toBeInTheDocument()
-    expect(headingElement.textContent).toBe(
-      '128GB Solid Storage Memory card - SanDisk Ultra'
+  it('should render the MetaData component', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      </Provider>
     )
+
+    const MetaDataComponent = screen.getByTestId('meta-data')
+    expect(MetaDataComponent).toBeInTheDocument()
   })
 
-  it('should render the number of reviews', () => {
-    const ratingElement = screen.getByText('(5 Reviews)')
-    expect(ratingElement).toBeInTheDocument()
-  })
-
-  it('should render price element', () => {
-    const priceElement = screen.getByText('$45.67')
-    expect(priceElement).toBeInTheDocument()
-  })
-
-  it('should render a view detail button', () => {
-    const viewDetailButton = screen.getByRole('link', { name: /Details/i })
-    expect(viewDetailButton).toBeInTheDocument()
-  })
-
-  it('should render the component with the correct title', async () => {
-    const pageTitle = 'The best products online - Ecommerce Amazon'
+  it('should render the Products component when there is data', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      </Provider>
+    )
 
     await waitFor(() => {
-      expect(document.title).toBe(pageTitle)
+      const ProductsComponent = screen.getByTestId('products')
+      expect(ProductsComponent).toBeInTheDocument()
+    })
+  })
+
+  it('should render the Loader component when the request is not finished', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    const LoaderComponent = screen.getByTestId('loader')
+    expect(LoaderComponent).toBeInTheDocument()
+  })
+
+  it('should call the tostify.error method when there is on error on the server', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    server.use(
+      rest.get('http://localhost:5125/api/v1/product/list', (req, res, ctx) => {
+        return res(
+          ctx.delay(1),
+          ctx.status(500),
+          ctx.json({ error: 'Error on the server' })
+        )
+      })
+    )
+
+    expect(toast.error).not.toHaveBeenCalled()
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalled()
+      expect(toast.error).toHaveBeenCalledTimes(1)
     })
   })
 })
