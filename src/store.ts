7c03fa3720f13eb31ef6fe1eef6ea428e9e4ce78
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-26 22:50:03 -0600

    chore: configuration of productPagination reducer

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productReducer } from './slices/productsSlice'
 import { productByIdReducer } from './slices/productByIdSlice'
+import { productPaginationReducer } from './slices/productPaginationSlice'
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
     product: productByIdReducer,
+    productPagination: productPaginationReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
