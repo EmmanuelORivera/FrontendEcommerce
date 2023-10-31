5525fa39f1226616b8b2184f1b1a495decc5f13b
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-31 16:38:31 -0600

    feat: Slider added to the Home component

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,13 +1,20 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks'
 import MetaData from './Layout/MetaData'
 import { getProductPagination } from '../actions/productsAction'
 import { toast } from 'react-toastify'
 import Products from './Products/Products'
 import Pagination from 'react-js-pagination'
-import { setPageIndex } from '../slices/productPaginationSlice'
+import { setPageIndex, updatePrice } from '../slices/productPaginationSlice'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+
+const { createSliderWithTooltip } = Slider
+const Range = createSliderWithTooltip(Slider.Range)
 
 const Home = () => {
+  const [price, setPrice] = useState([1, 10000])
+
   const dispatch = useAppDispatch()
   const {
     products,
@@ -26,6 +33,14 @@ const Home = () => {
 
   const setCurrentPageNumber = (pageNumber: number) => {
     dispatch(setPageIndex({ pageIndex: pageNumber }))
+  }
+
+  const onChangePrice = (priceEvent: number[]) => {
+    setPrice(priceEvent as number[])
+  }
+
+  const onAfterChange = (priceEvent: number[]) => {
+    dispatch(updatePrice({ price: priceEvent }))
   }
 
   useEffect(() => {
@@ -60,7 +75,31 @@ const Home = () => {
     <>
       <MetaData title="The best products online" />
       <section data-testid="home" id="products" className="container mt-5">
-        <Products products={products} col={4} loading={loading} />
+        {search ? (
+          <>
+            <div className="col-6 col-md-3 my-5">
+              <div className="px-5">
+                <Range
+                  className="test"
+                  marks={{ 1: `$1`, 10000: `$10000` }}
+                  min={1}
+                  max={10000}
+                  defaultValue={[1, 10000]}
+                  tipFormatter={(value) => `$${value}`}
+                  value={price}
+                  tipProps={{ placement: 'top', visible: true }}
+                  onChange={onChangePrice}
+                  onAfterChange={onAfterChange}
+                />
+              </div>
+            </div>
+            <div className="col-6 col-md-9">
+              <Products products={products} col={4} loading={loading} />
+            </div>
+          </>
+        ) : (
+          <Products products={products} col={4} loading={loading} />
+        )}
       </section>
 
       <div className="d-flex justify-content-center mt-5">
