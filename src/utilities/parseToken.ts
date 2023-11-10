33aac92249b98d4bfa75fa2adf389a10528f9098
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-09 22:45:57 -0600

    type: adding returning type to pareToken

--- src/utilities/parseToken.ts
+++ src/utilities/parseToken.ts
@@ -1,4 +1,7 @@
-export const parseToken = (token: string) => {
+interface JwtPayload {
+  [key: string]: any
+}
+export const parseToken = (token: string): JwtPayload => {
   const encodedPayload = token.split('.')[1]
   const urlSafePayload = encodedPayload.replace(/-/g, '+').replace(/_/g, '/')
   const decodedPayload = Buffer.from(urlSafePayload, 'base64').toString('utf-8')
