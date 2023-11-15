ea0ee7d3423348515fc227efd6ded3b4479c9e7b
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-15 11:01:54 -0600

    feat: creation NewPassword component

--- src/Components/Security/NewPassword.tsx
+++ src/Components/Security/NewPassword.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { useAppDispatch, useAppSelector } from '../../hooks'
+import { resetError } from '../../slices/resetPasswordSlice'
+import { resetPassword } from '../../actions/userAction'
+import MetaData from '../Layout/MetaData'
 
 const NewPassword = () => {
   const navigate = useNavigate()
@@ -15,42 +18,77 @@ const NewPassword = () => {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
+  useEffect(() => {
+    if (errors) {
+      errors.forEach((error) => toast.error(error))
+      dispatch(resetError())
+    }
+    if (message) {
+      toast.success(message)
+      dispatch(resetError())
+      navigate('/login')
+    }
+  }, [dispatch, toast, errors, message, navigate])
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (token)
+      dispatch(resetPassword({ email, password, confirmPassword, token }))
+  }
+
   return (
-    <div className="row wrapper">
-      <div className="col-10 col-lg-5">
-        <form className="shadow-lg">
-          <h1 className="mb-3">New Password</h1>
+    <>
+      <MetaData title="Reset Password" />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow-lg" onSubmit={submitHandler}>
+            <h1 className="mb-3">New Password</h1>
 
-          <div className="form-group">
-            <label htmlFor="password_field">Password</label>
-            <input
-              type="password"
-              id="password_field"
-              className="form-control"
-              value=""
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="email_field">Email</label>
+              <input
+                type="email"
+                id="email_field"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="confirm_password_field">Confirm Password</label>
-            <input
-              type="password"
-              id="confirm_password_field"
-              className="form-control"
-              value=""
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="password_field">Password</label>
+              <input
+                type="password"
+                id="password_field"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <button
-            id="new_password_button"
-            type="submit"
-            className="btn btn-block py-3"
-          >
-            Set Password
-          </button>
-        </form>
+            <div className="form-group">
+              <label htmlFor="confirm_password_field">Confirm Password</label>
+              <input
+                type="password"
+                id="confirm_password_field"
+                className="form-control"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              id="new_password_button"
+              type="submit"
+              className="btn btn-block py-3"
+              disabled={loading}
+            >
+              Reset Password
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
